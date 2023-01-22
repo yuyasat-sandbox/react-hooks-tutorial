764844be08204882d16a4662336caa6cf44cfc10
yuyasat <takiyour@gmail.com>
2023-01-22 16:20:33 +0900

    useRef, useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef, useReducer } from 'react';
 import './App.css';
 import ShinCodeContext from './main';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+}
 function App() {
   const [count, setCount] = useState(0);
   const shincodeInfo = useContext(ShinCodeContext)
+  const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
+
   const handleClick = () => {
     setCount(count + 1);
   };
@@ -17,6 +30,11 @@ function App() {
     console.log("Hello Hooks")
   }, [count])
 
+  const handleRef = () => {
+    console.log(ref.current.value)
+  }
+
+
   return (
     <div className="App">
       <h1>useState, useEffect</h1>
@@ -26,6 +44,14 @@ function App() {
       <h1>useContext</h1>
       <p>{shincodeInfo.name}</p>
       <p>{shincodeInfo.age}</p>
+      <h1>useRef</h1>
+      <input type="text" ref={ref} />
+      <button onClick={handleRef}>UseRef</button>
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+
     </div>
   )
 }
